aa59d40826c1ffe7dfbb09cb26e9d87787491b4b
gauuutier <152860198+gauuutier@users.noreply.github.com>
2025-10-16 14:11:02 +0200

    internship

--- CreateInputPlan4res.py
+++ CreateInputPlan4res.py
@@ -579,6 +579,7 @@ for current_scenario, current_year, current_option in product(cfg['scenarios'],c
 									for part in cfg['CouplingConstraints']['ActivePowerDemand']:
 										if vardict['Input']['Var'+typeData][part] in AnnualDataFrame['Variable'].unique():
 											valTS=valTS-AnnualDataFrame[ (AnnualDataFrame['Variable']==timeseriesdict['ZV'][part]) & (df['Region']==region) ][str(current_year)].unique()[0]
+							 
 							if valTS==0.0: valTS=cfg['ParametersCreate']['zerocapacity'] 
 							if firstSerie: 
 								newSerie=valTS*timeserie
@@ -1125,7 +1126,7 @@ for current_scenario, current_year, current_option in product(cfg['scenarios'],c
 						# MaxCapacity, if >0 defines the existing investment potential
 						TU['MaxAddedCapacity']= TU.apply(lambda row: row['MaxCapacity'] - row['Capacity'] if (row['Capacity'] < row['MaxCapacity'] and row['MaxCapacity'] < row['Capacity']+maxaddconfig) else maxaddconfig, axis=1)
 					elif 'MaxCapacity' in TU.columns:
-						TU['MaxAddedCapacity']= TU.apply(lambda row: row['MaxCapacity'] - row['Capacity'] if row['Capacity'] < row['MaxCapacity']  else 0, axis=1)
+						TU['MaxAddedCapacity']= TU.apply(lambda row: row['MaxCapacity'] - row['Capacity'] if (row['Capacity'] < row['MaxCapacity']  and row['MaxCapacity'] < 999999 )else 0, axis=1)
 					elif isMaxAddConfig:
 						TU['MaxAddedCapacity']= maxaddconfig
 					else:
@@ -1305,10 +1306,15 @@ for current_scenario, current_year, current_option in product(cfg['scenarios'],c
 						if (row in cfg['aggregateregions'] and SS.loc[row,'MaxVolume']>0 and SS.loc[row,'MaxPower']>cfg['ParametersCreate']['reservoir']['minpowerMWh']):
 							Rate=0
 							N=0
-							for reg in cfg['aggregateregions'][row]:
-								if reg in cfg['ParametersCreate']['InitialFillingrate']:
-									Rate=Rate+cfg['ParametersCreate']['InitialFillingrate'][reg]
-									N=N+1
+							if row in  cfg['ParametersCreate']['InitialFillingrate']:
+								Rate=Rate+cfg['ParametersCreate']['InitialFillingrate'][row]
+								N=1
+       
+							else :
+								for reg in cfg['aggregateregions'][row]:
+									if reg in cfg['ParametersCreate']['InitialFillingrate']:
+										Rate=Rate+cfg['ParametersCreate']['InitialFillingrate'][reg]
+										N=N+1
 							if N>0:
 								Rate=Rate/N
 							SS.loc[row,'InitialVolume']=SS.loc[row,'MaxVolume']*Rate
@@ -1498,7 +1504,10 @@ for current_scenario, current_year, current_option in product(cfg['scenarios'],c
 			STS['NumberUnits']=1	
 			STS['Inflows']=0	
 			STS['MinVolume']=0	
+				 
+											
 			STS['InitialVolume']=0	
+			STS['VolumeLevelTarget']=0
 			STS['Zone']=STS.index
 			if 'PrimaryRho' in STS.columns: STS['MaxPrimaryPower']=STS['MaxPower']*STS['PrimaryRho']
 			if 'SecondaryRho' in STS.columns: STS['MaxSecondaryPower']=STS['MaxPower']*STS['SecondaryRho']
@@ -1908,7 +1917,12 @@ for current_scenario, current_year, current_option in product(cfg['scenarios'],c
 				if row in timeseriesdict['RES'][oetechno].keys():
 					filetimeserie=timeseriesdict['RES'][oetechno][row]
 					RES.loc[row, 'MaxPowerProfile']=filetimeserie
-					
+					# compute energy scaling coefficient in inflows
+					timeserie=pd.read_csv(cfg['dirTimeSeries']+filetimeserie,header=0)
+					energy=timeserie[cfg['StochasticScenarios']].sum(axis=0)
+					Energy=energy.mean()
+					RES.loc[row, 'Energy_Timeserie']=Energy  
+	 
 			if v==0:
 				BigRES=RES
 				v=1
@@ -1928,6 +1942,7 @@ for current_scenario, current_year, current_option in product(cfg['scenarios'],c
 			if 'MaxAddedCapacity' in BigRES.columns: listcols.append('MaxAddedCapacity')
 			if 'MaxRetCapacity' in BigRES.columns: listcols.append('MaxRetCapacity')
 			if 'InvestmentCost' in BigRES.columns: listcols.append('InvestmentCost')
+			if 'Energy_Timeserie' in BigRES.columns: listcols.append('Energy_Timeserie')      
 
 		BigRES=BigRES[ listcols ]
 		BigRES=BigRES[ BigRES['Zone'].isin(cfg['partition'][partitionDemand]) ]
